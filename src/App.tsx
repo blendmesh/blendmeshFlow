5814efdf42c28108cb308a27b6d5278dd7481ee3
Tiago <atoumdevops@gmail.com>
2025-01-19 17:43:22 -0300

    toggle layout

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import { useViewportStore } from './store/viewport';
 import { useDndStore } from './store/Dnd';
 import { Panel } from '@xyflow/react';
 import NodePanel from './modules/nodePanel/NodePanel';
+import dagre from '@dagrejs/dagre';
 
 
 const initialNodes = [
@@ -35,6 +36,46 @@ const flowKey = 'example-flow';
 
 let id = 0;
 const getId = () => `dndnode_${id++}`;
+
+const dagreGraph = new dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+ 
+const nodeWidth = 172;
+const nodeHeight = 36;
+
+const getLayoutedElements = (nodes, edges, direction = 'TB') => {
+  const isHorizontal = direction === 'LR';
+  dagreGraph.setGraph({ rankdir: direction });
+ 
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+ 
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+ 
+  dagre.layout(dagreGraph);
+ 
+  const newNodes = nodes.map((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    const newNode = {
+      ...node,
+      targetPosition: isHorizontal ? 'left' : 'top',
+      sourcePosition: isHorizontal ? 'right' : 'bottom',
+      // We are shifting the dagre node position (anchor=center center) to the top left
+      // so it matches the React Flow node anchor point (top left).
+      position: {
+        x: nodeWithPosition.x - nodeWidth / 2,
+        y: nodeWithPosition.y - nodeHeight / 2,
+      },
+    };
+ 
+    return newNode;
+  });
+ 
+  return { nodes: newNodes, edges };
+};
+
 
 export default function App() {
   const {
@@ -182,6 +223,17 @@ export default function App() {
     [nodeType],
   );
 
+  const onLayout = useCallback(
+    (direction) => {
+      const { nodes: layoutedNodes, edges: layoutedEdges } =
+        getLayoutedElements(nodes, edges, direction);
+ 
+      setNodes([...layoutedNodes]);
+      setEdges([...layoutedEdges]);
+    },
+    [nodes, edges],
+  );
+
   return (
 
     <div style={{ width: '100vw', height: '100vh' }}>
@@ -209,6 +261,8 @@ export default function App() {
         { generalView.devtools && <DevTools />}
         { generalView.devtools && 
           <Panel position="top-center">
+            <button onClick={() => onLayout('TB')}>vertical layout</button>
+            <button onClick={() => onLayout('LR')}>horizontal layout</button>
             <button onClick={onSave}>save</button>
             <button onClick={onRestore}>restore</button>
             <button onClick={exportData}>download</button>
